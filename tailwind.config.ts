cecac10ff4b4c311351d677412b8ad546b9e396c
Wilbur Zhang <wilbur.zhaang@gmail.com>
2025-02-10 14:45:32 -0500

    init dummy text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        serif: ["var(--font-eb-garamond)", "serif"],
+      },
     },
   },
   plugins: [],
